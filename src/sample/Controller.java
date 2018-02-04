008818a110b70bca601c5f0ea2884af294ad7627
Ruszkabányai Ákos <akosruszka@gmail.com>
2018-02-04 11:14:13 +0100

    Project initialized

---
+++ src/sample/Controller.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    //<editor-fold desc="FXML annotations">
+    @FXML
+    Pane gamePane;
+    @FXML
+    Pane menuPane;
+    @FXML
+    Button newGameButton;
+    @FXML
+    Button gameContinueButton;
+    @FXML
+    Button helpButton;
+    @FXML
+    Button exitButton;
+    //</editor-fold>
+
+    private void setMenuPane() {
+        newGameButton.setOnAction(event -> {
+            showGamePane();
+            /* new Game object */
+        });
+        gameContinueButton.setOnAction(event -> {
+            /* initialized Game object load */
+        });
+        helpButton.setOnAction(event -> {
+            /* show help text in new Pane or popup window */
+        });
+        exitButton.setOnAction(event -> {
+            System.exit(0);
+        });
+    }
+
+    public void showGamePane() {
+        menuPane.setDisable(true);
+        menuPane.setVisible(false);
+        gamePane.setVisible(true);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setMenuPane();
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Pong Game");
+        primaryStage.setHeight(450);
+        primaryStage.setWidth(700);
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
